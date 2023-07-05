65d275c0de610580064c1528c525e32c42d99064
Yazmin Romo <yromo@cps.edu>
2023-07-05 14:37:16 -0500

    first commit

---
+++ src/Lab10.cpp
@@ -0,0 +1,51 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/Users/admin/Desktop/Remys_Labs/Lab10/src/Lab10.ino"
+void setup();
+void loop();
+#line 1 "/Users/admin/Desktop/Remys_Labs/Lab10/src/Lab10.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+void setup()
+{
+  pinMode(D5, OUTPUT);
+  pinMode(D7, INPUT);
+  Serial1.begin(9600);
+}
+
+void loop()
+{
+  if (Serial1.available() > 0)
+  {
+    char value = Serial1.read();
+
+    if (value == '?')
+    {
+      Serial.println("? Recieved");
+
+      bool buttonValue = digitalRead(D7);
+      if (buttonValue == true)
+      {
+        digitalWrite(D5, HIGH);
+        Serial1.print("X");
+      }
+      else if (buttonValue = false)
+      {
+        digitalWrite(D5, LOW);
+        Serial1.print("O");
+      }
+    }
+    else if (value == '0')
+    {
+      digitalWrite(D5, LOW);
+    }
+    else if (value == '1')
+    {
+      digitalWrite(D5, HIGH);
+    }
+  }
+}
